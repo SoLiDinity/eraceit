a012ca00d2877c5bf78bf3b67aaabb6ad7edc46b
Leonardo Denavito <jounandavito@gmail.com>
2024-11-04 00:51:42 +0700

    Added about route

--- app.py
+++ app.py
@@ -35,5 +35,9 @@ def upload_file():
     
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000, debug=True)
