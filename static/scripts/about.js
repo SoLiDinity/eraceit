81eafd4fc42e94c73914f9073dd2353acafe66ce
Leonardo Denavito <jounandavito@gmail.com>
2024-11-05 17:49:05 +0700

    Removing unecessary log

--- static/scripts/about.js
+++ static/scripts/about.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      console.log(entry);
-
       if (entry.isIntersecting) {
         entry.target.classList.add('show');
       }
